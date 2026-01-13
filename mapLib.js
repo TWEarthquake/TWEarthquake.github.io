2ab9f888573a684e8044fcce13bdcd5e68c2c54c
臺灣地震速報 <ccchang147852@gmail.com>
2026-01-13 10:53:01 +0800

    Update mapLib.js

--- mapLib.js
+++ mapLib.js
@@ -121,6 +121,8 @@ function updateColor(data) {
     if (colorLayer) {
         map.removeLayer(colorLayer);
     }
+    report.forEach(r => map.removeLayer(r));
+    report = [];
 
     colorLayer = L.geoJSON(taiwanGeoJSON, {
         style: feature => {
