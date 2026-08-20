d3a988064583d0c937cdd6cc3bf1f07178ee6bd4
臺灣地震速報 <ccchang147852@gmail.com>
2026-08-21 02:29:07 +0800

    Update eew.js

--- eew.js
+++ eew.js
@@ -211,10 +211,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     const isStandalone = window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true;
-    if (isStandalone) {
-        document.getElementById("setting").style.display = "none"
-        document.getElementById("settingTitle").style.display = "none"
-    }
     if (isStandalone && Notification.permission !== "granted") {
         document.getElementById("notificationButton").style.display = "block"
         alert("請點擊畫面右側的「開啟通知」，以確保能即時收到地震通知。")
